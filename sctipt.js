02f2fb1b4506f3c96a1a3966356101dad69095ad
PavanUmmagani07 <pavankalyanU@outlook.com>
2024-12-31 21:34:04 +0530

    LOOPS LINKEDINE

---
+++ sctipt.js
@@ -0,0 +1,40 @@
+/**
+ * LOOPS IN JAVASCRIPT
+ * 1 For Loop
+ * 
+ * for(initialisation;condition;increment/decrement){
+ *      //Block of code
+ * }
+ * 2 While Loop
+ * initialisation
+ * while(condition){
+ *  block of code
+ *  increment/decrement
+ * }
+ * 3 Do while
+ * initialisation
+ * do{
+ *  //block of code
+ * }while(condition)
+ * 
+ * LOOPS: Loops are used to execute the block of  Code repeatedly untill specified condition is true,
+ *        If the condition gets false, The Loops will get terminated
+ */
+
+
+// for(let i = 0; i<=10; i++){
+//     console.log(i);
+// }
+
+
+// let i = 0
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+let i  = 0;
+do{
+    console.log(i)
+    i++
+}while(i<0)
